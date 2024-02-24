e44a76e4660d71910dfa024dd62d4ec0843d5d57
RameshBellani <rameshbellani95@gmail.com>
2024-02-24 09:40:21 +0000

    ipl dashboard

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,22 @@
+// Write your code here
+import './index.css'
+
+const MatchCard = props => {
+  const {cards} = props
+  const {competingTeam, competingTeamLogo, matchStatus, result} = cards
+  const status = matchStatus === 'Won' ? 'win' : 'lose'
+
+  return (
+    <li className="cards-bg">
+      <img
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+        className="cards-logo"
+      />
+      <p className="card-name-heading">{competingTeam}</p>
+      <p className="card-name-para">{result}</p>
+      <p className={status}>{matchStatus}</p>
+    </li>
+  )
+}
+export default MatchCard
---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,19 @@
+// Write your code here
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const TeamCard = props => {
+  const {TeamCardItem} = props
+  const {id, name, teamImageUrl} = TeamCardItem
+
+  return (
+    <Link to={`/team-matches/${id}`} className={`item-link ${id}`}>
+      <li className="team-card-item-container">
+        <img src={teamImageUrl} alt={`${name}`} className="team-logo" />
+        <p className="team-logo-heading">{name}</p>
+      </li>
+    </Link>
+  )
+}
+export default TeamCard
